e81bc3b34115a857b412a8853fe65e28e540f01f
Peter Mackenzie-Helnwein <mackheln@gmail.com>
2018-10-21 19:41:19 -0700

    Prevent overwriting of existing animation files (performance enhancement)

--- images/makeAnimations.sh
+++ images/makeAnimations.sh
@@ -1,19 +1,13 @@
-#!/usr/bin/bash
+#!/bin/bash
 
 DELAY=25
 
-convert -delay $DELAY sdir015cm*png anim_sdir015cm.gif
-convert -delay $DELAY sdir020cm*png anim_sdir020cm.gif
-convert -delay $DELAY sdir025cm*png anim_sdir025cm.gif
-convert -delay $DELAY sdir030cm*png anim_sdir030cm.gif
-convert -delay $DELAY sdir040cm*png anim_sdir040cm.gif
-convert -delay $DELAY sdir050cm*png anim_sdir050cm.gif
-convert -delay $DELAY sdir062cm*png anim_sdir062cm.gif
-convert -delay $DELAY sdir075cm*png anim_sdir075cm.gif
-convert -delay $DELAY sdir087cm*png anim_sdir087cm.gif
-convert -delay $DELAY sdir100cm*png anim_sdir100cm.gif
-convert -delay $DELAY sdir125cm*png anim_sdir125cm.gif
-convert -delay $DELAY sdir150cm*png anim_sdir150cm.gif
-convert -delay $DELAY sdir200cm*png anim_sdir200cm.gif
-convert -delay $DELAY sdir300cm*png anim_sdir300cm.gif
-convert -delay $DELAY sdir500cm*png anim_sdir500cm.gif
+for SIZE in 015 020 025 030 040 050 062 075 087 100 125 150 200 300 500
+do
+    INFILES=sdir${SIZE}cm\*png
+    OUTFILE=anim_sdir${SIZE}cm.gif
+    if [ ! -f ${OUTFILE} ]
+    then
+        convert -delay ${DELAY} ${INFILES} ${OUTFILE}
+    fi
+done
