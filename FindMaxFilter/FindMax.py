1efa83d609dbad30c9db91e6964c7edb36ad0e08
Peter Mackenzie-Helnwein <pmackenz@uw.edu>
2018-10-22 09:04:47 -0700

    adjusting filter to our txt files

--- FindMaxFilter/FindMax.py
+++ FindMaxFilter/FindMax.py
@@ -15,8 +15,13 @@ nvals = 0
 linecount = 0
 
 for line in sys.stdin:
-    vals = [ float(x.strip()) for x in line.split('\t')]
+    svals = [ x.strip() for x in line.split('\t')]
     if linecount:
+        if ( len(svals) != nvals ):
+            print('line:', line)
+            print(svals)
+            raise ValueError
+        vals = [ float(x) for x in svals ]
         for i in range(nvals):
             if (vals[i] > maxlist[i]):
                 maxlist[i] = vals[i]
@@ -24,14 +29,14 @@ for line in sys.stdin:
                 minlist[i] = vals[i]
     else:
         # this is the first line
-        maxlist = vals[:]
-        minlist = vals[:]
-        nvals = len(vals)
+        nvals = len(svals) - 1 # the "- 1" is to fix a bug in our txt files
+        maxlist = [ -9999999. for i in range(nvals) ]
+        minlist = [ +9999999. for i in range(nvals) ]
     linecount += 1
 
-print('{} columns found:'.format{nvals})
+print('{} columns found:'.format(nvals))
 for i in range(nvals):
-    print('col {}: min={}   max={}'.format(i, minlist[i], maxlist[i]))
+    print('col {}: min={}   max={}'.format(i+1, minlist[i], maxlist[i]))
     
 
 
